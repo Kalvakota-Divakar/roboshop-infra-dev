71d9fc00398c991b8c1176dcb0493712acd8c58a
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-07-06 15:22:44 +0530

    ALB

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -16,3 +16,31 @@ resource "aws_lb" "backend_alb" {
   )
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>Hi, I am from HTTP Backend ALB</h1>"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+  
+  # load balancer details
+  alias {
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
